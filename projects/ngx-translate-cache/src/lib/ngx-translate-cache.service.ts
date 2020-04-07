41e4b1b8d9808817978c8d1c68066f53f7a4990b
Jaime <jaime.glez.pacheco@gmail.com>
2020-04-07 13:16:13 -0400

    DI issue in Angular 9 fixed (#24)

--- projects/ngx-translate-cache/src/lib/ngx-translate-cache.service.ts
+++ projects/ngx-translate-cache/src/lib/ngx-translate-cache.service.ts
@@ -1,10 +1,10 @@
-import { Inject, InjectionToken, Provider } from '@angular/core';
+import { Inject, Injectable, InjectionToken, Provider } from '@angular/core';
 import { TranslateService, TranslationChangeEvent } from '@ngx-translate/core';
 
-export type CacheMechanism = 'LocalStorage' | 'Cookie';
+export type CacheMechanismType = 'LocalStorage' | 'Cookie';
 export namespace CacheMechanism {
-  export const LocalStorage: CacheMechanism = 'LocalStorage';
-  export const Cookie: CacheMechanism = 'Cookie';
+  export const LocalStorage: CacheMechanismType = 'LocalStorage';
+  export const Cookie: CacheMechanismType = 'Cookie';
 }
 
 export const CACHE_NAME = new InjectionToken<string>('CACHE_NAME');
@@ -14,7 +14,7 @@ export const COOKIE_EXPIRY = new InjectionToken<string>('COOKIE_EXPIRY');
 export interface TranslateCacheConfig {
   cacheService: Provider;
   cacheName?: string;
-  cacheMechanism?: CacheMechanism;
+  cacheMechanism?: CacheMechanismType;
   cookieExpiry?: number;
 }
 
@@ -22,16 +22,15 @@ const DEFAULT_CACHE_NAME = 'lang';
 const DEFAULT_CACHE_MECHANISM = CacheMechanism.LocalStorage;
 const DEFAULT_COOKIE_EXPIRY = 720;
 
+@Injectable()
 export class TranslateCacheSettings {
-
   constructor(@Inject(CACHE_NAME) public cacheName: string = DEFAULT_CACHE_NAME,
-              @Inject(CACHE_MECHANISM) public cacheMechanism: CacheMechanism = DEFAULT_CACHE_MECHANISM,
+              @Inject(CACHE_MECHANISM) public cacheMechanism: string = DEFAULT_CACHE_MECHANISM,
               @Inject(COOKIE_EXPIRY) public cookieExpiry: number = DEFAULT_COOKIE_EXPIRY) {}
 }
 
 /* Not injectable */
 export class TranslateCacheService {
-
   constructor(private translateService: TranslateService,
               private translateCacheSettings: TranslateCacheSettings) {}
 
